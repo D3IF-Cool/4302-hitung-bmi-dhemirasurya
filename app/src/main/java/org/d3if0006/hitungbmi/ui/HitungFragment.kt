d7836dfd8e4d6d26658d09465fdf56057ab20e7b
anak_agung_ayu_surya_dhemira <dhemirasurya15@gmail.com>
2021-03-19 21:55:24 +0800

    Menambahkan navigasi sederhana

--- app/src/main/java/org/d3if0006/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if0006/hitungbmi/ui/HitungFragment.kt
@@ -7,20 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import org.d3if0006.hitungbmi.R
 import org.d3if0006.hitungbmi.databinding.FragmentHitungBinding
 
 class HitungFragment :Fragment() {
 
     private lateinit var binding: FragmentHitungBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHitungBinding.inflate(
             layoutInflater, container, false
         )
         binding.button.setOnClickListener { hitungBmi() }
+        binding.saranButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                R.id.action_hitungFragment_to_saranFragment
+            )
+        }
         return binding.root
     }
 
@@ -54,6 +59,7 @@ class HitungFragment :Fragment() {
 
         binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
         binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+        binding.saranButton.visibility = View.VISIBLE
     }
 
     private fun getKategori(bmi: Float, isMale: Boolean): String {
